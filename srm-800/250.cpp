3706fefd228d236f1f7c34b7de384e2f29f85762
Jorge Ktch <jorgektch@gmail.com>
2021-02-06 14:15:28 -0500

    SRM 800 prepared

---
+++ srm-800/250.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class ClassName{
+	public: long long FunctionName(vector <int> p)
+		
+        return
+	}
+};
+int main(){
+	ClassName obj;
+	cout << "Case 1: " << obj.FunctionName({0, 1, 2, 3}) << " | " << "31" << endl;
+	cout << "Case 2: " << obj.FunctionName({0, 1, 0, 3}) << " | " << "31" << endl;
+	cout << "Case 3: " << obj.FunctionName({0, 0, 0, 0, 0, 0}) << " | " << "43" << endl;
+	cout << "Case 4: " << obj.FunctionName({0, 0, 1, 1, 2}) << " | " << "49" << endl;
+	return 0;
+}
