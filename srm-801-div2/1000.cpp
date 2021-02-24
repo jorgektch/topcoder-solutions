ea9b5a7aed292ae676166a4249f12e9678e90688
Jorge Ktch <jorgektch@gmail.com>
2021-02-23 21:46:33 -0500

    SRM 801 prepared

---
+++ srm-801-div2/1000.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class ClassName{
+	public: long long FunctionName(vector <int> p)
+		
+        return
+	}
+};
+int main(){
+	ClassName obj;
+    cout << "Case 0: " << obj.FunctionName(input) << " | " << "output" << endl;
+    cout << "Case 1: " << obj.FunctionName(input) << " | " << "output" << endl;
+    cout << "Case 2: " << obj.FunctionName(input) << " | " << "output" << endl;
+    cout << "Case 3: " << obj.FunctionName(input) << " | " << "output" << endl;
+	return 0;
+}
